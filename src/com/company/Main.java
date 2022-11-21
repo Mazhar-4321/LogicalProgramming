69f65596fd8d71be26aaa272d1ece07343c82d80
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-21 14:32:31 +0530

    ADDED: Code to check a number is Perfect or not

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,18 +6,35 @@ public class Main {
     static final int ZERO = 0;
     static final int BINARY = 2;
     static final int FIBONACCI_SERIES = 1;
+    static final int PERFECT_NUMBER = 2;
     static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Logical Programming");
         System.out.println("Select Option to proceed");
         System.out.println("Enter 1 for Fibonnaci series");
+        System.out.println("Enter 2 for Perfect Number");
         int option = scanner.nextInt();
         switch (option) {
             case FIBONACCI_SERIES:
                 printFibonacciSeries();
                 break;
+            case PERFECT_NUMBER:
+                isPerfectNumber();
+                break;
         }
+    }
+
+    private static void isPerfectNumber() {
+        System.out.println("Enter a Number");
+        int number = scanner.nextInt();
+        int i = 1;
+        int factorsAggregate = 0;
+        while (i <= number / 2) {
+            factorsAggregate += (number % i == 0 ? i : 0);
+            i += 1;
+        }
+        System.out.println(number == factorsAggregate ? "Perfect Number" : "Not a Perfect Number");
     }
 
     private static void printFibonacciSeries() {
