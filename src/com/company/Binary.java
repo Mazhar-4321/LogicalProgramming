11bbaf6ebd8e8c54046fbefd0122fead32060548
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-21 14:22:37 +0530

    ADDED: Code to print Fibonacci series upto given N value

--- src/com/company/Binary.java
+++ src/com/company/Binary.java
@@ -2,12 +2,9 @@ package com.company;
 
 public class Binary {
     private static final int BINARY = 2;
-    private static final char CHAR_ZERO='0';
+    private static final char CHAR_ZERO = '0';
     private static final int ZERO = 0;
     private static final int ONE = 1;
-    private static final int FORWARD = 0;
-    private static final int REVERSE = 1;
-    private static final int OCTET = 8;
     private static final int HALF_BIT = 4;
     private static String binaryRepresentation = "";
     private static String swappedString = "";
@@ -28,8 +25,8 @@ public class Binary {
     }
 
     private void swapNibbles() {
-        if(binaryRepresentation.length()<8){
-            binaryRepresentation=0+binaryRepresentation;
+        if (binaryRepresentation.length() < 8) {
+            binaryRepresentation = ZERO + binaryRepresentation;
         }
         String nibble1 = binaryRepresentation.substring(ZERO, HALF_BIT);
         String nibble2 = binaryRepresentation.substring(HALF_BIT);
@@ -39,15 +36,13 @@ public class Binary {
 
     private void toDecimal(String binaryRepresentation) {
         int decimalRepresentation = ZERO;
-        for (int i = binaryRepresentation.length() - 1,k=ZERO; i >= ZERO; i--,k++) {
-            if(binaryRepresentation.charAt(i)==CHAR_ZERO){
-            decimalRepresentation= decimalRepresentation+(int)(Math.pow(2,k)*ZERO);
-            continue;
+        for (int i = binaryRepresentation.length() - 1, k = ZERO; i >= ZERO; i--, k++) {
+            if (binaryRepresentation.charAt(i) == CHAR_ZERO) {
+                decimalRepresentation = decimalRepresentation + (int) (Math.pow(2, k) * ZERO);
+                continue;
             }
-            decimalRepresentation= decimalRepresentation+(int)(Math.pow(2,k)*ONE);
+            decimalRepresentation = decimalRepresentation + (int) (Math.pow(2, k) * ONE);
         }
         System.out.println(decimalRepresentation);
-
-
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,39 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
     static final int ZERO = 0;
     static final int BINARY = 2;
+    static final int FIBONACCI_SERIES = 1;
+    static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Logical Programming");
-        toBinary(100);
-        Binary binary= new Binary(100);
+        System.out.println("Select Option to proceed");
+        System.out.println("Enter 1 for Fibonnaci series");
+        int option = scanner.nextInt();
+        switch (option) {
+            case FIBONACCI_SERIES:
+                printFibonacciSeries();
+                break;
+        }
+    }
 
+    private static void printFibonacciSeries() {
+        System.out.println("Enter a number to print fibonacci series");
+        int limit = scanner.nextInt();
+        int first = 0;
+        int second = 1;
+        System.out.print(first + " " + second + " ");
+        limit -= 2;
+        while (limit > ZERO) {
+            int temp = first + second;
+            System.out.print(temp + " ");
+            first = second;
+            second = temp;
+            limit -= 1;
+        }
     }
 
     public static void toBinary(int decimalNumber) {
@@ -19,15 +44,5 @@ public class Main {
         }
         System.out.println(binaryRepresentation);
     }
-    public static void findSqrt(int c){
-        if(c>0){
-            System.out.println("Invalid Input");
-            return;
-        }
-        int t=c;
-        double ans=0;//9
-//       while(Math.abs(t-c/t)>) {
-//           t = t + c / t;
-//       }
-    }
+
 }
