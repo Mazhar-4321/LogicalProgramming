b6b733e57d7407bd4fc2b76580f2881ec297164b
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-21 08:53:45 +0530

    ADDED: Code to find Binary Representation Of a Decimal Number

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,23 @@
 package com.company;
 
 public class Main {
+    static final int ZERO = 0;
+    static final int BINARY = 2;
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("Logical Programming");
+        toBinary(16);
+    }
+
+    public static void toBinary(int decimalNumber) {
+        String binaryRepresentation = "";
+        while (decimalNumber > ZERO) {
+            binaryRepresentation = (decimalNumber % BINARY) + binaryRepresentation;
+            decimalNumber = decimalNumber / BINARY;
+
+        }
+        System.out.println(binaryRepresentation);
+
     }
 }
